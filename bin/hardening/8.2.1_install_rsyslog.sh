80ac5bfe5b13a7c47a66f5b0fc22d30319870c17
Samson-W <sccxboy@gmail.com>
2019-04-22 23:58:47 +0800

    Add 8.2.1 for rsyslog and Adjust the use case number.

---
+++ bin/hardening/8.2.1_install_rsyslog.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#
+# harbian audit 9 Hardening
+#
+
+#
+# 8.2.1 Install the rsyslog package (Scored)
+# Author : Samson wen, Samson <sccxboy@gmail.com>
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+HARDENING_LEVEL=3
+
+# In debian, rsyslog has been chosen
+PACKAGE='rsyslog'
+PACKAGE_NG='syslog-ng'
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+    is_pkg_installed $PACKAGE_NG	
+    if [ $FNRET = 0 ]; then
+		ok "$PACKAGE_NG has installed, so pass."
+		FNRET=0
+    else
+    	is_pkg_installed $PACKAGE
+    	if [ $FNRET != 0 ]; then
+        	crit "$PACKAGE is not installed!"
+			FNRET=1
+    	else
+        	ok "$PACKAGE is installed."
+			FNRET=0
+    	fi
+	fi
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+        if [ $FNRET = 0 ]; then
+            ok "$PACKAGE is installed or $PACKAGE_NG is installed."
+        else
+            crit "$PACKAGE is absent, installing it."
+            apt_install $PACKAGE
+        fi
+}
+
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r $CIS_ROOT_DIR/lib/main.sh ]; then
+    . $CIS_ROOT_DIR/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi
