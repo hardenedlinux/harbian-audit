39da6d480aaa5c49d8c5839b50897cf74daac11b
Samson-W <sccxboy@gmail.com>
2019-05-14 04:07:26 +0800

    Add audit and apply methods for 12.4: Ensure permissions on /etc/gshadow are configured

---
+++ bin/hardening/12.4_etc_gshadow_permissions.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#
+# harbian audit 7/8/9  Hardening
+#
+
+#
+# 12.4 Verify Permissions on /etc/gshadow (Scored)
+# Author: Samson-W (sccxboy@gmail.com)
+#
+
+set -e # One error, it's over
+set -u # One variable unset, it's over
+
+HARDENING_LEVEL=1
+
+FILE='/etc/gshadow'
+PERMISSIONS='640'
+USER='root'
+GROUP='shadow'
+
+# This function will be called if the script status is on enabled / audit mode
+audit () {
+	has_file_correct_ownership $FILE $USER $GROUP
+	if [ $FNRET = 0 ]; then
+		ok "$FILE has correct ownership"
+	else
+		crit "$FILE ownership was not set to $USER:$GROUP"
+	fi
+    has_file_correct_permissions $FILE $PERMISSIONS
+    if [ $FNRET = 0 ]; then
+        ok "$FILE has correct permissions"
+    else
+        crit "$FILE permissions were not set to $PERMISSIONS"
+    fi 
+}
+
+# This function will be called if the script status is on enabled mode
+apply () {
+	has_file_correct_ownership $FILE $USER $GROUP
+	if [ $FNRET = 0 ]; then
+		ok "$FILE has correct ownership"
+	else
+		warn "fixing $FILE ownership to $USER:$GROUP"
+		chown $USER:$GROUP $FILE
+	fi
+    has_file_correct_permissions $FILE $PERMISSIONS
+    if [ $FNRET = 0 ]; then
+        ok "$FILE has correct permissions"
+    else
+        info "fixing $FILE permissions to $PERMISSIONS"
+        chmod 0$PERMISSIONS $FILE
+    fi
+}
+
+# This function will check config parameters required
+check_config() {
+    :
+}
+
+# Source Root Dir Parameter
+if [ -r /etc/default/cis-hardening ]; then
+    . /etc/default/cis-hardening
+fi
+if [ -z "$CIS_ROOT_DIR" ]; then
+     echo "There is no /etc/default/cis-hardening file nor cis-hardening directory in current environment."
+     echo "Cannot source CIS_ROOT_DIR variable, aborting."
+    exit 128
+fi
+
+# Main function, will call the proper functions given the configuration (audit, enabled, disabled)
+if [ -r $CIS_ROOT_DIR/lib/main.sh ]; then
+    . $CIS_ROOT_DIR/lib/main.sh
+else
+    echo "Cannot find main.sh, have you correctly defined your root directory? Current value is $CIS_ROOT_DIR in /etc/default/cis-hardening"
+    exit 128
+fi
