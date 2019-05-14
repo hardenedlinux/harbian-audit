b7623768826c252e134454bb64df5b11b055761a
Samson-W <sccxboy@gmail.com>
2019-05-14 17:48:49 +0800

    Fix a bug for 8.2.5: when syslog-ng has installed, pass this check item.

--- bin/hardening/8.2.5_rsyslog_remote_host.sh
+++ bin/hardening/8.2.5_rsyslog_remote_host.sh
@@ -15,27 +15,38 @@ set -u # One variable unset, it's over
 HARDENING_LEVEL=3
 
 PATTERN='^*.*[^I][^I]*@'
+PACKAGE_NG='syslog-ng'
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
-    FILES="$SYSLOG_BASEDIR/rsyslog.conf $SYSLOG_BASEDIR/rsyslog.d/*.conf"
-    does_pattern_exist_in_file "$FILES" "$PATTERN"
-    if [ $FNRET != 0 ]; then
-		crit "$PATTERN is not present in $FILES"
-    else
-        ok "$PATTERN is present in $FILES"
-    fi
+	is_pkg_installed $PACKAGE_NG	
+	if [ $FNRET = 0 ]; then
+		ok "$PACKAGE_NG has installed, so pass."
+	else
+    	FILES="$SYSLOG_BASEDIR/rsyslog.conf $SYSLOG_BASEDIR/rsyslog.d/*.conf"
+    	does_pattern_exist_in_file "$FILES" "$PATTERN"
+    	if [ $FNRET != 0 ]; then
+			crit "$PATTERN is not present in $FILES"
+    	else
+        	ok "$PATTERN is present in $FILES"
+   	 	fi
+	fi
 }
 
 # This function will be called if the script status is on enabled mode
 apply () {
-    FILES="$SYSLOG_BASEDIR/rsyslog.conf $SYSLOG_BASEDIR/rsyslog.d/*.conf"
-    does_pattern_exist_in_file "$FILES" "$PATTERN"
-    if [ $FNRET != 0 ]; then
-        crit "$PATTERN is not present in $FILES, please manual operation set a remote host to send your logs"
-    else
-        ok "$PATTERN is present in $FILES"
-    fi
+	is_pkg_installed $PACKAGE_NG	
+	if [ $FNRET = 0 ]; then
+		ok "$PACKAGE_NG has installed, so pass."
+	else
+    	FILES="$SYSLOG_BASEDIR/rsyslog.conf $SYSLOG_BASEDIR/rsyslog.d/*.conf"
+    	does_pattern_exist_in_file "$FILES" "$PATTERN"
+    	if [ $FNRET != 0 ]; then
+        	crit "$PATTERN is not present in $FILES, please manual operation set a remote host to send your logs"
+    	else
+        	ok "$PATTERN is present in $FILES"
+    	fi
+	fi
 }
 
 # This function will create the config file for this check with default values
