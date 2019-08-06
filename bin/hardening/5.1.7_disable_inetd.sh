29afbe24011bb1608a02ce7bb706ba7a5ca9143c
Samson-W <samson@hardenedlinux.org>
2019-08-06 17:09:17 +0800

    Modified 5.1.7 to be compatible with CentOS.

--- bin/hardening/5.1.7_disable_inetd.sh
+++ bin/hardening/5.1.7_disable_inetd.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #
-# harbian audit 7/8/9  Hardening
+# harbian audit 7/8/9/10 or CentOS Hardening
+# Modify by: Samson-W (samson@hardenedlinux.org)
 #
 
 #
-# 5.1.8 Ensure xinetd is not enabled (Scored)
+# 5.1.7 Ensure xinetd is not enabled (Scored)
 #
 
 set -e # One error, it's over
@@ -14,9 +15,13 @@ set -u # One variable unset, it's over
 HARDENING_LEVEL=3
 
 PACKAGES='openbsd-inetd xinetd rlinetd'
+PACKAGES_REDHAT='xinetd'
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
+	if [ $OS_RELEASE -eq 2 ]; then
+		PACKAGES=$PACKAGES_REDHAT
+	fi
     for PACKAGE in $PACKAGES; do 
         is_pkg_installed $PACKAGE
         if [ $FNRET = 0 ]; then
@@ -29,12 +34,19 @@ audit () {
 
 # This function will be called if the script status is on enabled mode
 apply () {
+	if [ $OS_RELEASE -eq 2 ]; then
+		PACKAGES=$PACKAGES_REDHAT
+	fi
     for PACKAGE in $PACKAGES; do 
         is_pkg_installed $PACKAGE
         if [ $FNRET = 0 ]; then
             warn "$PACKAGE is installed, purging"
-            apt-get purge $PACKAGE -y
-            apt-get autoremove
+			if [ $OS_RELEASE -eq 2 ]; then
+				yum remove $PACKAGE -y
+			else
+            	apt-get purge $PACKAGE -y
+            	apt-get autoremove
+			fi
         else
             ok "$PACKAGE is absent"
         fi
