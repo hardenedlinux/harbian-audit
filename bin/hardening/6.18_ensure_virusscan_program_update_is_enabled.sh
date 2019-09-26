2938a3075b554393065f0a90f428306d1595f505
Samson-W <samson@hardenedlinux.org>
2019-09-26 23:31:48 +0800

    Fix a bug for 6.18 and update how_to_creating_and_making_an_AMI_public.mkd

--- bin/hardening/6.18_ensure_virusscan_program_update_is_enabled.sh
+++ bin/hardening/6.18_ensure_virusscan_program_update_is_enabled.sh
@@ -13,48 +13,38 @@ set -e # One error, it's over
 set -u # One variable unset, it's over
 
 HARDENING_LEVEL=4
-VIRULSERVER='clamav-daemon'
 CLAMAVCONF_DIR='/etc/clamav/clamd.conf'
 UPDATE_SERVER='clamav-freshclam'
 
 # This function will be called if the script status is on enabled / audit mode
 audit () {
-    if [ $(systemctl | grep  $VIRULSERVER | grep "active running" | wc -l) -ne 1 ]; then
-        crit "$VIRULSERVER is not runing"
-        FNRET=1
-    else
-        ok "$VIRULSERVER is runing"
-        UPDATE_DIR=$(grep -i databasedirectory "$CLAMAVCONF_DIR" | awk '{print $2}')
-        if [ -d $UPDATE_DIR -a -e $CLAMAVCONF_DIR ]; then
-            NOWTIME=$(date +"%s")
-			# This file extension name maybe change to .cvd or .cld
-            VIRUSTIME=$(stat -c "%Y" "$UPDATE_DIR"/daily.*)
-            INTERVALTIME=$((${NOWTIME}-${VIRUSTIME}))
-            if [ "${INTERVALTIME}" -ge 604800 ];then
-                crit "Database file has a date older than seven days from the current date"
-                FNRET=3
-            else
-                ok "Database file has a date less than seven days from the current date"
-                FNRET=0
-            fi
-        else
-            crit "Clamav config file or update dir is not exist"
-            FNRET=2
-        fi
-    fi
+	UPDATE_DIR=$(grep -i databasedirectory "$CLAMAVCONF_DIR" | awk '{print $2}')
+	if [ -d $UPDATE_DIR -a -e $CLAMAVCONF_DIR ]; then
+		NOWTIME=$(date +"%s")
+		# This file extension name maybe change to .cvd or .cld
+		VIRUSTIME=$(stat -c "%Y" "$UPDATE_DIR"/daily.*)
+		INTERVALTIME=$((${NOWTIME}-${VIRUSTIME}))
+		if [ "${INTERVALTIME}" -ge 604800 ];then
+			crit "Clamav database file has a date older than seven days from the current date"
+			FNRET=3
+		else
+			ok "Clamav database file has a date less than seven days from the current date"
+			FNRET=0
+		fi
+	else
+		crit "Clamav config file or update dir is not exist"
+		FNRET=2
+	fi
 }
 
 # This function will be called if the script status is on enabled mode
 apply () {
     if [ $FNRET = 0 ]; then
-        ok "Database file has a date less than seven days from the current date"
-    elif [ $FNRET = 1 ]; then
-        warn "Install $VIRULSERVER"
-        apt-get install -y $VIRULSERVER
+        ok "Clamav database file has a date less than seven days from the current date"
     elif [ $FNRET = 2 ]; then
         warn "Clamav config file or update dir is not exist, please check that is exist or check config"
     elif [ $FNRET = 3 ]; then
-        warn "Database file has a date older than seven days from the current date, start clamav-freshclam.service to update"
+        warn "Clamav database file has a date older than seven days from the current date, start clamav-freshclam.service to update"
         apt-get install -y $UPDATE_SERVER
         systemctl start $UPDATE_SERVER
     fi
