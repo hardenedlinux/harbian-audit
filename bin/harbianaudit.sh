93031e98fe63e0fc0b5535ba4a41a3a6c9f69d30
Samson-W <samson@hardenedlinux.org>
2020-04-15 15:49:18 +0800

    Update harbianaudit.sh

--- bin/harbianaudit.sh
+++ bin/harbianaudit.sh
@@ -15,9 +15,3 @@ sed -i 's/^status=.*/status=enabled/' /opt/harbianaudit/etc/conf.d/8.4.2_aide_cr
 /opt/harbianaudit/bin/hardening.sh --apply --only 8.4.1
 /opt/harbianaudit/bin/hardening.sh --apply --only 8.4.2
 /opt/harbianaudit/bin/hardening.sh --apply --only 8.1.32
-NETINTERFACE=$(ip link | grep -v "link/.*" | grep -v -w "lo" | awk -F: '{print $2}' | tr "\n" " ")
-/opt/harbianaudit/bin/etc.iptables.rules.v4.sh $NETINTERFACE
-/opt/harbianaudit/bin/etc.iptables.rules.v6.sh $NETINTERFACE
-/sbin/iptables-save -f /etc/iptables/rules.v4
-/sbin/ip6tables-save -f /etc/iptables/rules.v6
-
